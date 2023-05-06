a75776e327ae3e5972f8aa2bcf2222b874808641
Fernando Iturbide <fernandoiturbide320@aragon.unam.mx>
2023-05-05 20:54:34 -0500

    Se agrega guia

---
+++ LeerArchivos/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner i=new Scanner(System.in);
+
+        try {
+            int a=10;
+            int b=Integer.parseInt(i.nextLine());
+            System.out.println("División: "+(a/b));
+        }catch (ArithmeticException e) {
+
+            System.out.println("No se puede dividir entre 0");
+        }catch (NumberFormatException nfe){
+            System.out.println("No capturaste el dato correctamente");
+        }finally {
+            System.out.println("Siempre se ejecuta");
+        }
+        System.out.println("Fin del programa!!!");
+    }
+}
